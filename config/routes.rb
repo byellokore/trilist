4bc18b996f40bcb3513b67e43c9c2c1d779dec8c
gabriel fontoura dos santos <gabrielfontoura@me.com>
2018-03-28 19:36:08 -0300

    Pre release

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/home',    to: 'static_pages#home'
+
+  get 'static_pages/contact'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
