742a675bde77d81b2975378cb60649d6a9c924de
gabriel fontoura dos santos <gabrielfontoura@me.com>
2019-04-01 22:08:50 -0400

    changed to webpacker fo js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,16 @@
 const { environment } = require('@rails/webpacker')
+const erb =  require('./loaders/erb')
 
+environment.loaders.prepend('erb', erb)
+const webpack = require('webpack');
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        jquery: 'jquery',
+        'window.jQuery': 'jquery',
+        Popper: ['popper.js', 'default']
+    })
+)
 module.exports = environment
