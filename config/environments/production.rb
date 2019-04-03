3c16b4eb7c7b951ae3b3b2ba861ac2f2218680a7
gabriel fontoura dos santos <gabrielfontoura@me.com>
2019-04-02 21:59:09 -0400

    send grid student pack changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,9 +72,9 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDGRID_USERNAME'],
-      :password => ENV['SENDGRID_PASSWORD'],
-      :domain => 'trilist.herokuapp.com',
+      :user_name => "apikey",
+      :password => ENV['SEND_GRID_PASSWORD'],
+      :domain => 'trilist.com.br',
       :address => 'smtp.sendgrid.net',
       :port => 587,
       :authentication => :plain,
