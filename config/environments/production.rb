86c9a6ef0ad6b52a5f1001e4755addc535bf6e7c
gabriel fontoura dos santos <gabrielfontoura@me.com>
2019-04-02 21:36:24 -0400

    send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,12 +72,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :port           => ENV['MAILGUN_SMTP_PORT'],
-      :address        => ENV['MAILGUN_SMTP_SERVER'],
-      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-      :domain         => 'sandbox7e7d8a3eb35e41cc8a6385632bdad7a1.mailgun.org',
-      :authentication => :plain,}
+      :user_name => ENV['SENDGRID_USERNAME'],
+      :password => ENV['SENDGRID_PASSWORD'],
+      :domain => 'trilist.herokuapp.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
+      :enable_starttls_auto => true}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
