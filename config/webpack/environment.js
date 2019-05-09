188e35231a287e9b030f3c3424ff44924ab07faa
gabriel fontoura dos santos <gabrielfontoura@me.com>
2019-05-09 00:05:22 -0400

    erb loader removed

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,5 @@
 const { environment } = require('@rails/webpacker')
-const erb =  require('./loaders/erb')
 
-environment.loaders.prepend('erb', erb)
 const webpack = require('webpack');
 environment.plugins.prepend(
     'Provide',
